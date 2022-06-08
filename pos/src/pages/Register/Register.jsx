56e5981f7e5859d026027e6552612e322ce907ec
unknown <mahefaniainarindra@gmail.com>
2022-06-08 23:31:24 +0300

    Register Page

--- pos/src/pages/Register/Register.jsx
+++ pos/src/pages/Register/Register.jsx
@@ -1,9 +1,28 @@
 import './Register.css'
 import React from 'react'
+import welcome from '../../imgs/welcome blue.jpg'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='Register'>
+        <div className="register-header">
+            <a href="http://www.freepik.com">Designed by Freepik</a>
+            <img src={welcome} alt="" />
+        </div>
+
+        <div className="register-form">
+            <div className="register-user">
+                <input type="text" placeholder='Nom et Prénoms'/>
+                <input type="text" placeholder='Pseudonyme'/>
+                <input type="password" placeholder='Mot de Passe'/>
+                <input type="password" placeholder='Confirmer'/>
+            </div>
+
+            <div className="create-user">
+                <button class='register-btn'>Créer Compte</button>
+            </div>
+        </div>  
+    </div>
   )
 }
 
